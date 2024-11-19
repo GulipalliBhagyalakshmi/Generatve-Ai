5b8b681703ffbf667b79181c633e1114ff0437eb
GulipalliBhagyalakshmi <bhagyalaxmigullipalli@gmail.com>
2024-11-19 23:22:08 +0530

    first commit

---
+++ Complete-Python-Bootcamp-main/5-Modules/package/subpackages/mult.py
@@ -0,0 +1,4 @@
+def multiply(a,b):
+    return a*b
+def division(a,b):
+    return a-b*a
---
+++ Complete-Python-Bootcamp-main/5-Modules/test.py
@@ -0,0 +1,8 @@
+from package.maths import *
+from package.subpackages.mult import multiply
+from package.subpackages.mult import division
+
+print(addition(2,3))
+print(substraction(4,3))
+print(multiply(6,5)) 
+print(division(3,3))
